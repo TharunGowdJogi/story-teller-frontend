06f3842402b050b40b234ef280ddd38138a174b7
TharunGowdJogi <128439253+TharunGowdJogi@users.noreply.github.com>
2024-05-17 14:00:17 -0400

    Add basic files

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import MenuBar from "./components/MenuBar.vue";
+</script>
+
+<template>
+  <v-app :style="{ background: '#F4F4F4' }">
+    <MenuBar :key="$route.fullPath" />
+    <h1> Welcome to Story Teller Frontend</h1>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
