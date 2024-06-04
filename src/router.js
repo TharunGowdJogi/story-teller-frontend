25da8674c7720df63e038d65a2716c753bea0f20
sumarayi4 <suma.rayi@eagles.oc.edu>
2024-06-04 01:34:46 -0400

    Displaying the story in new component

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 const create_story = () => import('./views/create_story.vue');
+const display_story = () => import('./views/display_story.vue');
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,6 +19,11 @@ const router = createRouter({
       path: '/create_story',
       name: 'create_story',
       component: create_story,
+    },
+    {
+      path: '/display_story/:id',
+      name: 'display_story',
+      component: display_story,
     },
   ],
 });
