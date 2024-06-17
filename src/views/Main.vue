5e38925e13931fac80ab2a9a6f99924aef79c546
sumarayi4 <suma.rayi@eagles.oc.edu>
2024-06-17 18:30:59 -0400

    Revert "Add recommendations and user history (#13)"
    This reverts commit 3391a2e3a6d616fdcd224c34e0850ec4c432c104.

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -4,8 +4,6 @@ import { useRouter } from "vue-router";
 import { ref } from "vue";
 import PageLoader from "../components/PageLoader.vue";
 import Alert from "../components/Alert.vue";
-import Recommendations from "../components/recommendations.vue"
-import UserHistory from "../components/UserHistory.vue"
 
 const isLoading = ref(true);
 const router = useRouter();
@@ -22,18 +20,51 @@ onMounted(async () => {
     router.push({ name: "login" });
   }
 });
+
 </script>
 
 <template>
 <v-container>
-    <UserHistory />
-    <Recommendations />
+  <div class="container" style="margin-top: 20px">
+    <div style="display: flex;">
+    </div>
+    <br/>
     <PageLoader v-if="isLoading" />
   <Alert :snackbar="snackbar"/>
   <br/>
+  </div>
 </v-container>
 </template>
 
 <style scoped>
+.dashboard {
+    background-color: white;
+    padding: 30px;
+}
+.col-6{
+    margin-bottom: 50px;
+}
+.dashboard-static-data {
+    display: flex;
+    width: 40%;
+    justify-content: space-between;
+}
+.static {
+    margin-top: 40px;
+}
 
+table, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+table {
+    width: 80%;
+}
+td {
+    width: 80%;
+    padding: 10px;
+}
+th {
+    padding: 10px;
+}
 </style>
--- src/views/display_story.vue
+++ src/views/display_story.vue
@@ -1,20 +1,19 @@
 <template>
   <v-container>
     <div>
-      <v-card v-if="story.story_id">
-        <v-card-title>{{ story.title }}</v-card-title>
-        <v-card-subtitle>{{ formatDate(story.created_at) }}</v-card-subtitle>
-        <v-card-text>{{ story.text }}</v-card-text>
-        <v-divider></v-divider>
-        <v-card-actions v-if="story.story_genre != null">
-          <v-chip>{{ story.story_genre?.genre_name }}</v-chip>
-          <v-chip>{{ story.story_country?.country_name }}</v-chip>
-          <v-chip>{{ story.story_role?.role_name }}</v-chip>
-          <v-chip>{{ story.story_language?.language_name }}</v-chip>
-        </v-card-actions>
-      </v-card>
-      <br/>
-      <v-btn @click="downloadPDF" color="primary" class="mb-4">Get For Offline</v-btn>
+    <v-card v-if="story.story_id">
+      <v-card-title>{{ story.title }}</v-card-title>
+      <v-card-subtitle>{{ formatDate(story.created_at) }}</v-card-subtitle>
+      <v-card-text>{{ story.text }}</v-card-text>
+      <v-divider></v-divider>
+      <v-card-actions v-if="story.story_genre != null">
+        <v-chip>{{ story.story_genre?.genre_name }}</v-chip>
+        <v-chip>{{ story.story_country?.country_name }}</v-chip>
+        <v-chip>{{ story.story_role?.role_name }}</v-chip>
+        <v-chip>{{ story.story_language?.language_name }}</v-chip>
+      </v-card-actions>
+    </v-card>
+    <br/>
     </div>
     <v-alert v-if="errorMessage" type="error">{{ errorMessage }}</v-alert>
   </v-container>
@@ -24,7 +23,6 @@
 import { ref, onMounted } from 'vue';
 import { useRoute } from 'vue-router';
 import bedtimeStoryServices from '../services/bedtime_story_services.js';
-import html2pdf from 'html2pdf.js'; 
 
 const route = useRoute();
 const story = ref({});
@@ -36,17 +34,6 @@ onMounted(async () => {
     try {
       const {data} = await bedtimeStoryServices.get_story(storyId);
       story.value = data;
-
-      // Add story ID to localStorage for recent read stories
-      const recentStories = JSON.parse(localStorage.getItem('recentStories')) || [];
-      if (!recentStories.includes(storyId)) {
-        recentStories.unshift(storyId);
-        if (recentStories.length > 10) {
-          recentStories.pop();
-        }
-        localStorage.setItem('recentStories', JSON.stringify(recentStories));
-      }
-
     } catch (error) {
       errorMessage.value = error.message;
     }
@@ -54,18 +41,6 @@ onMounted(async () => {
     errorMessage.value = 'Invalid story ID';
   }
 });
-
-const downloadPDF = () => {
-  const element = document.querySelector(".v-card");
-  const opt = {
-    margin:       0.5,
-    filename:     `${story.value.title}.pdf`,
-    image:        { type: 'jpeg', quality: 0.98 },
-    html2canvas:  { scale: 2 },
-    jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
-  };
-  html2pdf().from(element).set(opt).save();
-};
 
 const formatDate = (dateString) => {
   const options = { year: 'numeric', month: 'long', day: 'numeric' };
