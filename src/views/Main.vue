3391a2e3a6d616fdcd224c34e0850ec4c432c104
sumarayi4 <suma.rayi@eagles.oc.edu>
2024-06-17 18:30:40 -0400

    Add recommendations and user history (#13)

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -4,6 +4,8 @@ import { useRouter } from "vue-router";
 import { ref } from "vue";
 import PageLoader from "../components/PageLoader.vue";
 import Alert from "../components/Alert.vue";
+import Recommendations from "../components/recommendations.vue"
+import UserHistory from "../components/UserHistory.vue"
 
 const isLoading = ref(true);
 const router = useRouter();
@@ -20,51 +22,18 @@ onMounted(async () => {
     router.push({ name: "login" });
   }
 });
-
 </script>
 
 <template>
 <v-container>
-  <div class="container" style="margin-top: 20px">
-    <div style="display: flex;">
-    </div>
-    <br/>
+    <UserHistory />
+    <Recommendations />
     <PageLoader v-if="isLoading" />
   <Alert :snackbar="snackbar"/>
   <br/>
-  </div>
 </v-container>
 </template>
 
 <style scoped>
-.dashboard {
-    background-color: white;
-    padding: 30px;
-}
-.col-6{
-    margin-bottom: 50px;
-}
-.dashboard-static-data {
-    display: flex;
-    width: 40%;
-    justify-content: space-between;
-}
-.static {
-    margin-top: 40px;
-}
 
-table, th, td {
-  border: 1px solid black;
-  border-collapse: collapse;
-}
-table {
-    width: 80%;
-}
-td {
-    width: 80%;
-    padding: 10px;
-}
-th {
-    padding: 10px;
-}
 </style>
